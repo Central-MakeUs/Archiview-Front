18dba4069b270d5885d54983db4034f8b5a3f024
MinjoonHK <minjoon.park.hk@gmail.com>
2026-01-07 20:46:29 +0900

    🎨 STYLE: 리뷰 내용 반영
    - letter spacing 추가
    - typo 이름 피그마와 동일하게 변경
    - reset css 제거 -> Tailwind 에 내장되어 있음

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,12 +2,13 @@ export default function Home(): React.ReactNode {
   return (
     <div>
       <div className="flex justify-center items-center h-screen flex-col">
-        <div className="h1">Archiview Project Initialize</div>
-        <div className="h2">Archiview Project Initialize</div>
-        <div className="h3">Archiview Project Initialize</div>
-        <div className="b1">Archiview Project Initialize</div>
-        <div className="b2">Archiview Project Initialize</div>
-        <div className="b3">Archiview Project Initialize</div>
+        <div className="heading1">Archiview Project Initialize</div>
+        <div className="heading2">Archiview Project Initialize</div>
+        <div className="heading3">Archiview Project Initialize</div>
+        <div className="body1">Archiview Project Initialize</div>
+        <div className="body2">Archiview Project Initialize</div>
+        <div className="body3">Archiview Project Initialize</div>
+        <div className="captions">Archiview Project Initialize</div>
         <div className="b1 text-neutral-50">Archiview Neutral Color</div>
         <div className="b1 text-primary-50">Archiview Primary Color</div>
         <div className="b1 text-secondary-50">Archiview Secondary Color</div>
