bc50692d9f4eee29ce46f735ef4b1b607262ee36
MinjoonHK <minjoon.park.hk@gmail.com>
2026-02-15 21:32:25 +0900

    🐛 FIX: 장소 상세페이지 이동 시 placeId 전달 수정

--- apps/web/src/pages/editor/home/ui/EditorPlaceItemList.tsx
+++ apps/web/src/pages/editor/home/ui/EditorPlaceItemList.tsx
@@ -13,6 +13,7 @@ export const EditorPlaceItemList = ({
         //TODO: 상세페이지 조회 API 나오면 상세페이지로 route 변경 및 데이터 연결
         <Link href={`/editor/register-place?placeId=${place.placeId}`} key={place.placeId}>
           <EditorPlaceItem
+            placeId={place.placeId}
             name={place.placeName}
             description={place.editorSummary}
             savedCount={place.stats.saveCount}
